3171e5edd152e4843fbb531830989a94496c17d4
Riski Kukuh Wiranata <riskikukuh.me@gmail.com>
2021-08-25 15:18:34 +0700

    user registration implemented
    Adding User registration

--- src/server.js
+++ src/server.js
@@ -2,11 +2,15 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
+const users = require('./api/users');
 const NotesService = require('./services/postgres/NotesService');
+const UsersService = require('./services/postgres/UsersService');
 const NotesValidator = require('./validator/notes/index');
+const UsersValidator = require('./validator/users/index');
 
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -22,6 +26,14 @@ const init = async () => {
     options: {
       service: notesService,
       validator: NotesValidator,
+    },
+  });
+
+  await server.register({
+    plugin: users,
+    options: {
+      service: usersService,
+      validator: UsersValidator,
     },
   });
 
